725cbf533575bfcc7fe3157cc1eec00bf30601ee
Natthawit Janjaroen <natwit442@gmail.com>
2020-01-14 09:30:07 +0700

    delete unrelated file

--- examples/voteapp/src/main/kotlin/com/natwit442/example/voteapp/voteapp/model/Vote.kt
+++
@@ -1,5 +0,0 @@
-package com.natwit442.example.voteapp.voteapp.model
-
-data class Vote(val voterId: String, val voteFor: String)
-
-
--- examples/voterPython/run_basic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-FLASK_APP=voteApp.py flask run --host=0.0.0.0
