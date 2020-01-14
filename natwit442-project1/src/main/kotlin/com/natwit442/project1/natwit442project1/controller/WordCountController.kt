101012436cbf6ed60aa7b0329321613178e95627
Natthawit Janjaroen <natwit442@gmail.com>
2020-01-14 10:18:00 +0700

    Handle each accept header properly

--- natwit442-project1/src/main/kotlin/com/natwit442/project1/natwit442project1/controller/WordCountController.kt
+++ natwit442-project1/src/main/kotlin/com/natwit442/project1/natwit442project1/controller/WordCountController.kt
@@ -7,14 +7,38 @@ import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 class WordCountController(val wordCountService: WordCountService) {
 
     private val LOGGER = LoggerFactory.getLogger(WordCountController::class.java)
+
+
+    data class Result(val sumOfWords: Int, val topTenWords: List<String>)
+
+    @RequestMapping("/wc", produces = ["application/json"])
+    @ResponseBody
+    fun processJson(@RequestParam(value="target")  url : String): Result {
+        val (sumOfWords, topTen) = wordCountService.countWord(url)
+        return Result(sumOfWords, topTen)
+    }
+
+    @RequestMapping("/wc", produces = ["text/plain"])
+    @ResponseBody
+    fun processPlainText(@RequestParam(value="target")  url : String): String {
+        LOGGER.info("Request comes for [plain text")
+        val (sumOfWords, topTen) = wordCountService.countWord(url)
+        return Result(sumOfWords, topTen).toString()
+    }
+
+
+
+
 
     @RequestMapping("/wc")
     fun computeAdd(@RequestParam(value="target")  url : String, model: Model) : String {
