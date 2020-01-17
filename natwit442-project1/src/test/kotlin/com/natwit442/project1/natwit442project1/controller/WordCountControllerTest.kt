42b5496c6c7757c69209250cf3eeecdc2d47be5f
natwit442 <natwit442@gmail.com>
2020-01-17 13:16:03 +0700

    MVC Testing

--- natwit442-project1/src/test/kotlin/com/natwit442/project1/natwit442project1/controller/WordCountControllerTest.kt
+++ natwit442-project1/src/test/kotlin/com/natwit442/project1/natwit442project1/controller/WordCountControllerTest.kt
@@ -1,27 +1,21 @@
 package com.natwit442.project1.natwit442project1.controller
 
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.http.*
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest()
+@AutoConfigureMockMvc
 internal class WordCountControllerTest {
 
-
-    @LocalServerPort
-    private val port = 0
-
     @Autowired
-    private val testRestTemplate: TestRestTemplate? = null
-
-    @Autowired
-    private val wordCountController: WordCountController? = null
+    private val mockMvc: MockMvc? = null
 
 
     @Test
@@ -29,12 +23,11 @@ internal class WordCountControllerTest {
 
         //and do I need this JSON media type for my use case?
         //and do I need this JSON media type for my use case?
-        assertThat(testRestTemplate).isNotNull
-        val url = "http://localhost:${port}/wc?target=https://spring.io/guides/gs/testing-web/"
-        val headers = HttpHeaders()
-        val entity = HttpEntity<Any>(headers)
-        val out: ResponseEntity<String> = testRestTemplate!!.exchange<String>(url, HttpMethod.GET, entity, String::class.java)
-        assertThat(out.statusCode).isEqualTo(HttpStatus.OK)
+        mockMvc?.perform(get("/wc?target=https://spring.io/guides/gs/testing-web/"))?.andExpect(status().isOk)
+        mockMvc?.perform(get("/wc?target=https://spring.io/guides/gs/testing-web/"))?.andExpect(status().isOk)
+        mockMvc?.perform(get("/wc?target=https://spring.io/guides/gs/testing-web/"))?.andExpect(status().isOk)
+        mockMvc?.perform(get("/wc?target=https://spring.io/guides/gs/testing-web/"))?.andExpect(status().isOk)
+
 
     }
 
